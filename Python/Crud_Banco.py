0e81bb7b1323ea7cbc31a4d45aa649a6f7f645e7
ThiagoThmaz <kasuma17satto@gmail.com>
2024-11-06 21:30:00 -0300

    Feat: Corrigindo import da Exception

--- Python/Crud_Banco.py
+++ Python/Crud_Banco.py
@@ -1,6 +1,6 @@
 import json
 import oracledb
-from ErrorBanco import ErrorBanco
+from ErrorBanco import ErroBanco
 from Banco import conectar_db
 
 def inserir_registro(conn):
@@ -15,4 +15,4 @@ def inserir_registro(conn):
         conn.commit()
         print("Registro inserido com sucesso.")
     except Exception as e:
-       raise ErrorBanco(e)
+       raise ErroBanco(e)
